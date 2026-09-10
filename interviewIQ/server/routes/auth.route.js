47a96e57c87bb3df1364826668585945a1ba2796
gufranhasib20-droid <gufranhasib20@gmail.com>
2026-09-10 11:56:42 +0530

    Update auth.route.js

--- interviewIQ/server/routes/auth.route.js
+++ interviewIQ/server/routes/auth.route.js
@@ -4,6 +4,6 @@ import { googleAuth, logOut } from "../controllers/auth.controller.js"
 const authRouter=express.Router()
 
 authRouter.post("/google",googleAuth)
-authRouter.get("/logout",logOut)
+authRouter.post("/logout",logOut)
 
 export default authRouter
